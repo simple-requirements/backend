68fad8ee35f3ced7ff5776b7e8661f4d70b29758
Florian Weidinger <crasyhorse@gmx.net>
2026-06-11 19:13:51 +0000

    build: Switch to json format

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  const port = Number(process.env.PORT ?? 3000);
+    const port = Number(process.env.PORT ?? 3000);
 
-  await app.listen(port, '0.0.0.0');
+    await app.listen(port, '0.0.0.0');
 }
 
-bootstrap();
+void bootstrap();
