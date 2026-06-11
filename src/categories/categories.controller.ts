c8ad8f163a14f61385fdd7a7bd411465010e01f6
Florian Weidinger <crasyhorse@gmx.net>
2026-06-11 19:18:33 +0000

    feature: Add categories controller

---
+++ src/categories/categories.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import type { CategoryResponseDto } from './dto/category-response.dto';
+import type { CreateCategoryDto } from './dto/create-category.dto';
+import { CategoriesService } from './categories.service';
+
+@Controller('categories')
+export class CategoriesController {
+    constructor(private readonly categoriesService: CategoriesService) {}
+
+    @Post()
+    async create(@Body() createCategoryDto: CreateCategoryDto): Promise<CategoryResponseDto> {
+        return this.categoriesService.create(createCategoryDto);
+    }
+
+    @Get()
+    async findAll(): Promise<CategoryResponseDto[]> {
+        return this.categoriesService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string): Promise<CategoryResponseDto> {
+        return this.categoriesService.findOne(id);
+    }
+}
