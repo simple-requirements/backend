f62eeaf97ef393d64aff2695c7fab9175f14dde7
Florian Weidinger <crasyhorse@gmx.net>
2026-06-11 19:17:35 +0000

    feature: Add categories module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,43 +4,41 @@ import { TypeOrmModule, type TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
 import databaseConfig from './database/database.config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [databaseConfig],
-    }),
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true, load: [databaseConfig] }),
 
-    TypeOrmModule.forRootAsync({
-      inject: [databaseConfig.KEY],
+        TypeOrmModule.forRootAsync({
+            inject: [databaseConfig.KEY],
 
-      useFactory: (
-        database: ConfigType<typeof databaseConfig>,
-      ): TypeOrmModuleOptions => ({
-        type: 'postgres',
+            useFactory: (database: ConfigType<typeof databaseConfig>): TypeOrmModuleOptions => ({
+                type: 'postgres',
 
-        host: database.host,
-        port: database.port,
-        database: database.database,
-        username: database.username,
-        password: database.password,
+                host: database.host,
+                port: database.port,
+                database: database.database,
+                username: database.username,
+                password: database.password,
 
-        autoLoadEntities: true,
+                autoLoadEntities: true,
 
-        // Schema changes are managed through migrations.
-        synchronize: false,
-        migrationsRun: false,
+                // Schema changes are managed through migrations.
+                synchronize: false,
+                migrationsRun: false,
 
-        retryAttempts: 10,
-        retryDelay: 3_000,
+                retryAttempts: 10,
+                retryDelay: 3_000,
 
-        logging: false,
-      }),
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+                logging: false,
+            }),
+        }),
+
+        CategoriesModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
